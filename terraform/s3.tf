8b4fb1769169116ebce8435bde6b3bd9f0937ec3
RaphaelBRodrigues <raphaelbarbosa.rodrigues@gmail.com>
2026-09-14 22:45:10 -0300

    fix: make Terraform state bucket unique

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,3 +1,5 @@
+data "aws_caller_identity" "current" {}
+
 resource "aws_s3_bucket" "terraform_state" {
-  bucket = "garage-flow-terraform-state"
+  bucket = "garage-flow-${data.aws_caller_identity.current.account_id}-terraform-state"
 }
